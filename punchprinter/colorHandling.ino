dd27d6f310bd8ea2969650cdef47772ea3f7a03a
javl <mail@jaspervanloenen.com>
2015-02-04 01:25:45 +0100

    Latest version before first event

--- punchprinter/colorHandling.ino
+++ punchprinter/colorHandling.ino
@@ -1,48 +1,65 @@
 void calculate_colors(const int pot_data[4]){
-
+  //  Serial.println("Go create color");
   // new_colors[0] = left brightness
   // new_colors[1] = right brightness
   // new_colors[2] = right hue
   // new_colors[3] = left hue
 
-  HSVtoRGBtoCMYK(0, map(pot_data[3], 0, 1023, 0, 360), 100, map(pot_data[0], 0, 1023, 0, 100));
-  HSVtoRGBtoCMYK(NUMBER_OF_GLASSES-1, map(pot_data[2], 0, 1023, 0, 360), 100, map(pot_data[1], 0, 1023, 0, 100));
-
+  HSVtoRGBtoCMYK(0, map(pot_data[3], 0, 1023, 0, 360), 100, map(pot_data[0], 0, 1023, 5, 100));
+  HSVtoRGBtoCMYK(10-1, map(pot_data[2], 0, 1023, 0, 360), 100, map(pot_data[1], 0, 1023, 5, 100));
   // Get change per glass for each color: (last color - first color) / (number of glasses - 1)
-  float change_per_glass_C = (generated_colors[7][1] - generated_colors[0][1]) / (NUMBER_OF_GLASSES - 1);
-  float change_per_glass_M = (generated_colors[7][2] - generated_colors[0][2]) / (NUMBER_OF_GLASSES - 1);
-  float change_per_glass_Y = (generated_colors[7][3] - generated_colors[0][3]) / (NUMBER_OF_GLASSES - 1);
-  float change_per_glass_K = (generated_colors[7][4] - generated_colors[0][4]) / (NUMBER_OF_GLASSES - 1);
+  float change_per_glass_C = (generated_colors[9][0] - generated_colors[0][0]) / 9;
+  float change_per_glass_M = (generated_colors[9][1] - generated_colors[0][1]) / 9;
+  float change_per_glass_Y = (generated_colors[9][2] - generated_colors[0][2]) / 9;
+  float change_per_glass_K = (generated_colors[9][3] - generated_colors[0][3]) / 9;
 
   // Set all the seperate glass values (begincolor + n*change_per_glass
-  for(int i=1; i < NUMBER_OF_GLASSES -1; i++){
+  for(int i=1; i < 10 -1; i++){
     generated_colors[i][0] = round(generated_colors[i-1][0] + change_per_glass_C);
     generated_colors[i][1] = round(generated_colors[i-1][1] + change_per_glass_M);
     generated_colors[i][2] = round(generated_colors[i-1][2] + change_per_glass_Y);
     generated_colors[i][3] = round(generated_colors[i-1][3] + change_per_glass_K);
   }
+  //  Serial.println("Make color is done");
+  /*  Serial.println("GENERATED COLORS");
+   for(int k=0;k<4; k++){
+   for(int i=0;i<10;i++){
+   Serial.print(generated_colors[i][k]);
+   Serial.print("\t");
+   }
+   Serial.println("");
+   }
+   */
 }
 
+void HSVtoRGBtoCMYK(int store_in_index, float h, float s, float v){
+  h += 120.0;
+  if(h > 360.0){
+    h -= 360.0; 
+  }
+  h = 360.0 - h;
 
-void HSVtoRGBtoCMYK(int store_in_index, float h, float s, float v) {
-  // FIRST PART, HSV to RGB
+  //h = 200.0;
+  //s = 100.0;
+  //v = 50.0;
+
   h /= 360.0;
   s /= 100.0;
   v /= 100.0;
 
-  int i;
-  float f, p, q, t;
-  float r = 0;
-  float g = 0;
-  float b = 0;
+  float i, f, p, q, t;
+  float r = 0.0;
+  float g = 0.0;
+  float b = 0.0;
 
-  i = floor(h * 6);
-  f = h * 6 - i;
-  p = v * (1 - s);
-  q = v * (1 - f * s);
-  t = v * (1 - (1 - f) * s);
+  i = floor(h * 6.0);
 
-  switch (round(i % 6)) {
+  f = h * 6.0 - i;
+  p = v * (1.0 - s);
+  q = v * (1.0 - f * s);
+  t = v * (1.0 - (1.0 - f) * s);
+
+  switch (round(int(i) % 6)) {
   case 0: 
     r = v; 
     g = t; 
@@ -74,35 +91,179 @@ void HSVtoRGBtoCMYK(int store_in_index, float h, float s, float v) {
     b = q; 
     break;
   }
-  r = max(0, floor(r*255));
-  g = max(0, floor(r*255));
-  b = max(0, floor(r*255));
 
-  // SECOND PART, RGB to CMYK
+  r = max(0.0, min(255.0, float(round(r*255.0))));
+  g = max(0.0, min(255.0, float(round(g*255.0))));
+  b = max(0.0, min(255.0, float(round(b*255.0))));
+
+  float computedC = 0.0;
+  float computedM = 0.0;
+  float computedY = 0.0;
+  float computedK = 0.0;
 
   // BLACK
-  if (r==0 && g==0 && b==0) {
-    generated_colors[store_in_index][0] = 0;
-    generated_colors[store_in_index][1] = 0;
-    generated_colors[store_in_index][2] = 0;
-    generated_colors[store_in_index][3] = 100;
+  if (r==0.0 && g==0.0 && b==0.0) {
+    computedK = 1.0;
+    //    float[] output = {
+    //      0.0, 0.0, 0.0, 1.0
+    //    };
+    Serial.println("ALL BLACK");
     return;
   }
 
-  float color_C = 1 - (r/255);
-  float color_M = 1 - (g/255);
-  float color_Y = 1 - (b/255);
- 
-  float minCMY = min(color_C, min(color_M, color_Y));
-  color_C = (color_C - minCMY) / (1 - minCMY) ;
-  color_M = (color_M - minCMY) / (1 - minCMY) ;
-  color_Y = (color_Y - minCMY) / (1 - minCMY) ;
-  float color_K = minCMY;
+  computedC = 1.0 - (r/255.0);
+  computedM = 1.0 - (g/255.0);
+  computedY = 1.0 - (b/255.0);
 
-  generated_colors[store_in_index][0] = max(0, floor(color_C * 100));
-  generated_colors[store_in_index][1] = max(0, floor(color_M * 100));
-  generated_colors[store_in_index][2] = max(0, floor(color_Y * 100));
-  generated_colors[store_in_index][3] = max(0, floor(color_K * 100));
+  float minCMY = min(computedC, min(computedM, computedY));
+  computedC = (computedC - minCMY) / (1.0 - minCMY) ;
+  computedM = (computedM - minCMY) / (1.0 - minCMY) ;
+  computedY = (computedY - minCMY) / (1.0 - minCMY) ;
+  computedK = minCMY;
+
+  //float[] output = {
+  //  round(computedC*100), round(computedM*100), round(computedY*100), round(computedK*100)
+  //  };
+  //  Serial.println(computedC*100.0);
+  //  Serial.println(computedM*100.0);
+  //  Serial.println(computedY*100.0);
+  //  Serial.println(computedK*100.0);
+
+  generated_colors[store_in_index][0] = computedC*100;
+  generated_colors[store_in_index][1] = computedM*100;
+  generated_colors[store_in_index][2] = computedY*100;
+  generated_colors[store_in_index][3] = computedK*100;
 
 }
+
+/*void HSVtoRGBtoCMYK(int store_in_index, float h, float s, float v) {
+ // FIRST PART, HSV to RGB
+ h /= 360.0;
+ s /= 100.0;
+ v /= 100.0;
+ 
+ h = 90.0;
+ s = 100.0;
+ v = 100.0;
+ 
+ Serial.println("HSV IN: ");
+ Serial.print(h);
+ Serial.print(", ");  
+ Serial.print(s);
+ Serial.print(", ");  
+ Serial.print(v);
+ Serial.println("");
+ 
+ float i;
+ float f, p, q, t;
+ float r = 0;
+ float g = 0;
+ float b = 0;
+ 
+ i = floor(h * 6.0);
+ f = h * 6.0 - i;
+ p = v * (1 - s);
+ q = v * (1 - f * s);
+ t = v * (1.0 - (1.0 - f) * s);
+ switch (round(int(i) % 6)) {
+ case 0: 
+ r = v; 
+ g = t; 
+ b = p; 
+ break;
+ case 1: 
+ r = q; 
+ g = v; 
+ b = p; 
+ break;
+ case 2: 
+ r = p; 
+ g = v; 
+ b = t; 
+ break;
+ case 3: 
+ r = p; 
+ g = q; 
+ b = v; 
+ break;
+ case 4: 
+ r = t; 
+ g = p; 
+ b = v; 
+ break;
+ case 5: 
+ r = v; 
+ g = p; 
+ b = q; 
+ break;
+ }
+ Serial.print("R: ");
+ Serial.println(r);
+ Serial.print("G: ");
+ Serial.println(r);
+ Serial.print("B: ");
+ Serial.println(r);
+ r = max(0.0, floor(r*255.0));
+ g = max(0.0, floor(r*255.0));
+ b = max(0.0, floor(r*255.0));
+ 
+ // SECOND PART, RGB to CMYK
+ 
+ // BLACK
+ if (r==0.0 && g==0.0 && b==0.0) {
+ generated_colors[store_in_index][0] = 0.0;
+ generated_colors[store_in_index][1] = 0.0;
+ generated_colors[store_in_index][2] = 0.0;
+ generated_colors[store_in_index][3] = 100.0;
+ Serial.println("JUST BLACK");
+ return;
+ }
+ 
+ float color_C = 1.0 - (r/255.0);
+ float color_M = 1.0 - (g/255.0);
+ float color_Y = 1.0 - (b/255.0);
+ 
+ float minCMY = min(color_C, min(color_M, color_Y));
+ color_C = 100.0 * ((color_C - minCMY) / (1.0 - minCMY));
+ color_M = 100.0 * ((color_M - minCMY) / (1.0 - minCMY));
+ color_Y = 100.0 * ((color_Y - minCMY) / (1.0 - minCMY));
+ float color_K = 100.0 * minCMY;
+ 
+ Serial.println("CMYK COLORS: ");
+ Serial.println(color_C);
+ Serial.println(color_M);
+ Serial.println(color_Y);
+ Serial.println(color_K);
+ 
+ generated_colors[store_in_index][0] = max(0.0, floor(color_C));
+ generated_colors[store_in_index][1] = max(0.0, floor(color_M));
+ generated_colors[store_in_index][2] = max(0.0, floor(color_Y));
+ generated_colors[store_in_index][3] = max(0.0, floor(color_K));
+ 
+ float total_colors = color_C + color_M + color_Y + color_K;
+ 
+ Serial.println("CMYK: ");
+ Serial.println(generated_colors[store_in_index][0]);
+ Serial.println(generated_colors[store_in_index][1]);
+ Serial.println(generated_colors[store_in_index][2]);
+ Serial.println(generated_colors[store_in_index][3]);
+ // Prepare topping pump times, but set to a minus value so they won't run yet
+ //pump_timers[4] = -1 * (color_C / (total_colors / 100.0));
+ //pump_timers[5] = -1 * (color_M / (total_colors / 100.0));
+ //pump_timers[6] = -1 * (color_Y / (total_colors / 100.0));
+ //pump_timers[7] = -1 * (color_K / (total_colors / 100.0));
+ //Serial.println("Set topping timers: ");
+ //Serial.println(pump_timers[4]);
+ //Serial.println(pump_timers[5]);
+ //Serial.println(pump_timers[6]);
+ //Serial.println(pump_timers[7]);
+ }
+ */
+
+
+
+
+
+
+
 
--- punchprinter/punchprinter.ino
+++ punchprinter/punchprinter.ino
@@ -3,6 +3,21 @@
 //==============================================================
 // #define replaces keywords with their values during compilation
 
+// Convert the low color values (range 0-100) to useable run times for the pumps
+// milliseconds to run = color value * PIGMENT_PUMP_TIME_FACTOR
+#define PIGMENT_PUMP_TIME_FACTOR 35.0 // Don't forget the decimal!
+
+// How long the topping pumps should run in total (and so how much topping fluid
+// to use. This time will be proportionally devided between the needed topping pumps.
+#define TOPPING_PUMP_TOTAL_TIME 6000.0 // Don't forget the decimal!
+
+// The print button gets blocked for time (in milliseconds) to prevent printing double.
+#define BUTTON_BLOCK_PERIOD 5000.0
+
+// The potentiometer values are checked multiple times and an average of the measured
+// values is used to smooth the results. This sets the sample amount:
+//#define POT_SAMPLE_AMOUNT 10
+
 // Topping mode:
 // 1: do pigments, when done add topping
 // 2: do pigments, after TOPPING_DELAY (see below) also add topping
@@ -11,116 +26,126 @@
 #define TOPPING_MODE 1
 #define TOPPING_DELAY 1000.0 // only used by TOPPING_MODE 2
 
-// Convert the low color values (range 0-100) to useable run times for the pumps
-// milliseconds to run = color value * PIGMENT_PUMP_TIME_FACTOR
-#define PIGMENT_PUMP_TIME_FACTOR 10.0 // Don't forget the decimal!
-
-// How long the topping pumps should run in total (and so how much topping fluid
-// to use. This time will be proportionally devided between the needed topping pumps.
-#define TOPPING_PUMP_TOTAL_TIME 2000.0 // Don't forget the decimal!
-
-// The print button gets blocked for time (in milliseconds) to prevent printing double.
-#define BUTTON_BLOCK_PERIOD 1500.0
-
-// The potentiometer values are checked multiple times and an average of the measured
-// values is used to smooth the results. This sets the sample amount:
-#define POT_SAMPLE_AMOUNT 10
-
 //==============================================================
 // Setup the Arduino pins to use
 //==============================================================
-#define NUMBER_OF_GLASSES 10
+//#define NUMBER_OF_GLASSES 10
 
 // Printer states:
 // 0: just plugged in, waiting for all switches to go LOW
 // 1: all switches are low, toggle 0 to 7 to run the pumps
 // 2: normal print mode!
-volatile int printer_state = 0;  // Volatile so it can be changed in interrupt
+int printer_state = 0; // 0 = waiting for testmode, 1 = testmode, 2 = printmode
+// pump cmyk fill cmyk
+//int relay_pins[8] = { 
+//  2, 3, 4, 5, 6, 7, 8, 9 }; // Pins for the pump relays
+int relay_pins[8] = { 
+  5, 4, 3, 2, 9, 8, 7, 6 }; // Pins for the pump relays
+int switch_pins[10] = { 
+  22, 24, 26, 28, 30, 32, 34, 36, 38, 40 }; // Switches
+int pot_pins[4] = { 
+  0, 1, 2, 3 }; // Potentiometers
+int button_pin = 18; // Print button, 21 because it can use interrupts
+boolean print_button_is_available = true;
 
-int relay_pins[8] = { 2, 3, 4, 5, 6, 7, 8, 9 }; // Pins for the pump relays
-int switch_pins[10] = { 22, 24, 26, 28, 30, 32, 34, 36, 38, 40 }; // Switches
-int pot_pins[4] = { 0, 1, 2, 3 }; // Potentiometers
-int button_pin = 21; // Print button, 21 because it can use interrupts
+//int pot_readings[4][POT_SAMPLE_AMOUNT]; // the readings from the pots
+//int pot_index = 0; // the index of the current potreading
+//int pot_total[4] = {
+//  0, 0, 0, 0}; // the running total for the pots
+int pot_readings[4] = {
+  0, 0, 0, 0}; // Holds the final averaged pot values, used for colors
 
-int pot_readings[4][POT_SAMPLE_AMOUNT]; // the readings from the pots
-int pot_index = 0; // the index of the current potreading
-int pot_total[4] = {0, 0, 0, 0}; // the running total for the pots
-int pot_average[4] = {0, 0, 0, 0}; // Holds the final averaged pot values, used for colors
+boolean switch_states[] = { 
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }; // Keep track of switch positions
 
-boolean switch_states[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }; // Keep track of switch positions
-
-long timers[8]; // Timers keep track of time pumps run
+long pump_timers[8]; // Timers keep track of time pumps run
+long time_until_topping = 0; // How long to wait before adding topping
+long check_input_timer = 0; // Used to generate the color every second
 
 int cup_index = 0;  // The next cup to pour
 boolean can_dispend = true; // Whether pressing the button will dispend anything
 long button_block_timer = 0; // COunts back to zero when button is blocked after pressing
 
-float generated_colors[10][4]; // Holds the generated colors in 2D matrix ([cup][color])
+int generated_colors[10][4]; // Holds the generated colors in 2D matrix ([cup][color])
 
 //==============================================================
 // Setup
 //==============================================================
 void setup(){
-  // Add some hardcoded colors for testing
-  generated_colors[0][0] = 45.0;
-  generated_colors[0][1] = 45.0;
-  generated_colors[0][2] = 45.0;
-  generated_colors[0][3] = 45.0;
-
-  generated_colors[7][0] = 0.0;
-  generated_colors[7][1] = 11.0;
-  generated_colors[7][2] = 91.0;
-  generated_colors[7][3] = 10.0;
-
-  Serial.begin(115200);
+  ////Serial.begin(115200); // Start a serial connection to PC
 
   // Pump relays are active low so bring them HIGH to set to
   // their off position.
   for(int i=0;i<10;i++){
+    pinMode(switch_pins[i], INPUT);
+  }
+  pinMode(button_pin, INPUT);
+
+
+  for(int i=0;i<8;i++){
     pinMode(relay_pins[i], OUTPUT);
     digitalWrite(relay_pins[i], HIGH);
   }
 
   // Set all potentiometer readings to 0
-  for(int i=0;i<4;i++){
-    for(int k=0;k<10;k++){
-      pot_readings[i][k] = 0;
-    } 
-  }
+  //  for(int i=0;i<4;i++){
+  //    for(int k=0;k<10;k++){
+  //      pot_readings[i][k] = 0;
+  //    } 
+  //  }
 
-  // run the print_button_pressed when the print button is RELEASED
-  attachInterrupt(0, print_button_pressed, FALLING); 
- }
+}
 
 //==============================================================
 // Main loop
 //==============================================================
 void loop(){
-    // Wait for all the switches to go OFF before going to the next phase
+  // At the start the printer is in printer_state 0: it won't do
+  // anything until all of the switches are set to their OFF positions
+  // When this happens, it changes to printer_state 1
+  if(button_block_timer == 0){
+    if(digitalRead(button_pin) == 1){
+      // used to make sure a button needs to be depressed before recognizing it again
+      if(print_button_is_available){ 
+        print_button_is_available = false;
+        print_button_pressed(); 
+      }else{
+        print_button_is_available = true; 
+      }
+    }
+  }else{
+   print_button_is_available = false; 
+  }
+
   if(printer_state == 0){
     int switches_on = 0;
-    for(int i=0;i<8;i++){
-      switches_on += digitalRead(switch_pins[i]);
+    for(int i=0;i<10;i++){
+      if(digitalRead(switch_pins[i]) == 1){
+        switches_on++;
+      }
     }
     if(switches_on == 0){
-       printer_state = 1;
+      ////Serial.println("Switching to printer_state 1");
+      printer_state = 1;
     }
+
     // Turn on pumps when switches are set to ON
-  }else if(printer_state == 1){
-    for(int i=0;i<8;i++){
+  }
+  else if(printer_state == 1){
+    for(int i=0;i<8;i++){ // Check 8 pins (not 10) because there are 8 pumps
       if(digitalRead(switch_pins[i]) == HIGH){
         digitalWrite(relay_pins[i], LOW);
-      }else{
+      }
+      else{
         digitalWrite(relay_pins[i], HIGH);      
       }
     }
-  // Regular print mode
-  }else if(printer_state == 2){
-
+    // Regular print mode
   }
-
-  handle_input(); // Check switch / potmeter states
-  handle_serial(); // Check if Serial commands came in
+  else if(printer_state == 2){
+    // Don't do anything. Input is checked every second using check_input_timer
+  }
+  ////handle_serial(); // Check if Serial commands came in
   check_timers();  // Check is running timers have expired
 }
 
@@ -128,69 +153,141 @@ void loop(){
 // Check and handle changes in switch/pot positions
 //==============================================================
 void handle_input(){
-  // Check the switch states
+  // Save the switch states
   for(int i=0;i<10;i++){
     switch_states[i] = digitalRead(switch_pins[i]);
   }
 
-  // Check the potentiometers. An average will be calculated
-  boolean pot_values_changed = false;
-  for(int i=0;i<4;i++){
-    pot_total[i] = pot_total[i] - pot_readings[i][pot_index];         
-    pot_readings[i][pot_index] = analogRead(pot_pins[i]); 
-    pot_total[i]= pot_total[i] + pot_readings[i][pot_index];       
+  pot_readings[0] = max(0, 1023-analogRead(pot_pins[0])); // invert
+  pot_readings[1] = max(0, 1023-analogRead(pot_pins[1])); // invert
+  pot_readings[2] = analogRead(pot_pins[2]);
+  pot_readings[3] = analogRead(pot_pins[3]);
 
-    int old_average = pot_average[i];
-    pot_average[i] = pot_total[i] / POT_SAMPLE_AMOUNT;         
-    if(old_average != pot_average[i]){
-      pot_values_changed = true;
-    }
-  }
-  pot_index++;
-  if (pot_index >= POT_SAMPLE_AMOUNT){
-    pot_index = 0;
-  }    
-  // Only update the colors if pot values actually changed
-  if(pot_values_changed){
-    calculate_colors(pot_average);
-  }
+  calculate_colors(pot_readings);
+
 }
-
+/*
 void handle_serial(){
-  if(Serial.available() > 0){
-    int input = Serial.read() - '0'; // Convert ASCI code to number
-    // Do stuff when needed
-  }
-}
-
+ if(Serial.available() > 0){
+ int serial_input = Serial.read() - '0'; // Convert ASCI code to number
+ switch(serial_input){
+ case 0:
+ Serial.println("");
+ Serial.println("Printer_state:");
+ Serial.println(printer_state);
+ Serial.println("");
+ Serial.println("Printer button:");
+ Serial.println(digitalRead(button_pin));
+ Serial.println("Switches:");
+ for(int i=0;i<10;i++){
+ //        int switchState = digitalRead(switch_pins[i]);
+ Serial.print(digitalRead(switch_pins[i]));
+ Serial.print(" ");
+ }
+ Serial.println("");
+ Serial.println("Averages Left (hue / darkness) ");
+ Serial.println(pot_readings[3]);
+ Serial.println(pot_readings[0]);
+ Serial.println("Averages Right (hue / darkness) ");
+ Serial.println(pot_readings[2]);
+ Serial.println(pot_readings[1]);
+ Serial.println("Color values: ");
+ for(int i=0;i<NUMBER_OF_GLASSES;i++){
+ for(int k=0;k<4;k++){
+ Serial.print(generated_colors[i][k]);
+ Serial.print("\t");
+ }
+ Serial.println("");
+ }
+ break;
+ case 1:
+ Serial.println("Switching to printer_state 1 (manually)");
+ printer_state = 1;
+ break;
+ case 2:
+ 
+ Serial.println("Switching to printer_state 2 (manually)");
+ printer_state = 2;
+ check_input_timer = millis() + 1000;
+ break;
+ case 9:
+ print_button_pressed();
+ break;
+ }
+ // Do stuff when needed
+ }
+ }
+ */
 //==============================================================
 // Executed when print button is pressed
 //==============================================================
 void print_button_pressed(){
-  if(button_block_timer == 0){
-    // Turn on / off pumps based on switches 1 - 8
-    if(printer_state == 1){
+  if(button_block_timer == 0){ // First check if button is enabled
+    if(printer_state == 1){ // In pump test mode. Use switches to turn
+      // on the pumps. Press the print button to go to print mode
       int switches_on = 0;
-      for(int i=0;i<8;i++){
-        switches_on = digitalRead(switch_pins[i]);
+      for(int i=0;i<10;i++){
+        if(digitalRead(switch_pins[i]) == 1){
+          switches_on++;
+        }
       }
       if(switches_on == 0){
+        ////Serial.println("Switching to printer_state 2");
         printer_state = 2;
-      }
-    }else if(printer_state == 2){
-      dispend(cup_index);
-      if(cup_index < 10){
-        for(int i=cup_index+1;i<10;i++){
-          if(digitalRead(switch_pins[i]) == HIGH){
-            cup_index = i;
-            break;
-          }
-        }
-        // If this part gets reached, all selected cups are filles
-        // Block the button for a while
-        button_block_timer = millis() + (BUTTON_BLOCK_PERIOD * 2);
+        check_input_timer = millis() + 1000;      
       }
     }
+    else if(printer_state == 2){ // Normal print mode
+      int total_on = 0;
+      for(int i=0; i<10;i++){
+        if(digitalRead(switch_pins[i]) == 1){
+          total_on++;
+        }
+      }
+      if(total_on == 0){
+        cup_index = 0; 
+        ////Serial.println("RESET");
+        return;
+      }
+
+      // Make sure it loops around to 0
+      if(cup_index >= 10){
+        cup_index = 0; 
+      }
+      ////Serial.println("GA PRINTEN");
+      ////Serial.println(cup_index);
+      for(int i=cup_index; i<10;i++){
+        if(digitalRead(switch_pins[i]) == 1){
+          ////Serial.print("WEL AAN EN PRINT: ");
+          ////Serial.println(i);
+          ////Serial.println("dispend: ");
+          ////Serial.println(i);
+          dispend(i);
+          cup_index=i+1;
+          if(cup_index < 10){
+            boolean last_switch_reached = true;
+            for(int k=cup_index; k<10;k++){
+              if(digitalRead(switch_pins[k]) == 1){
+                last_switch_reached = false;
+                break;
+              } 
+            }
+            if(last_switch_reached){
+              ////Serial.println("TERUG NAAR NUL");             
+              cup_index = 0; 
+            }
+          }
+          break;
+        }
+        ////else{
+        ////  Serial.print("NIET AAN: "); 
+        ////  Serial.println(i); 
+        ////}
+
+      }      
+    }
+    // Block the button for a while
+    button_block_timer = millis() + BUTTON_BLOCK_PERIOD;
   }
 }
 
@@ -198,13 +295,12 @@ void print_button_pressed(){
 // Check if any of the timers have expired
 //==============================================================
 void check_timers (){
-
   // Pump timers
   for(uint8_t i=0;i<8;i++){
-    if(timers[i] != 0){
-      if(timers[i] < millis()){
+    if(pump_timers[i] > 0){
+      if(pump_timers[i] < millis()){
         digitalWrite(relay_pins[i], HIGH);
-        timers[i] = 0;
+        pump_timers[i] = 0;
       }
     }
   }
@@ -213,71 +309,75 @@ void check_timers (){
   if(button_block_timer > 0){
     if(button_block_timer < millis()){
       button_block_timer = 0;
+    }
+  }
+
+  if(time_until_topping > 0){
+    if(time_until_topping < millis()){
+      time_until_topping = 0;
+      for(int i=4;i<8;i++){
+        if(abs(pump_timers[i]) > 0){
+          pump_timers[i] = millis() + ((float(abs(pump_timers[i]))/100.0) * TOPPING_PUMP_TOTAL_TIME);
+          digitalWrite(relay_pins[i], LOW);
+        }
+      }
+    }
+  }
+
+  if(printer_state == 2){
+    if(check_input_timer < millis()){
+      handle_input();
+      check_input_timer = millis() + 1000;
     } 
   }
+
 }
 
 //==============================================================
 // Open the right tubes
 //==============================================================
 void dispend(const int index){
-  /*
-  float period = -1.0;
-  float longest_period = 0.0; // 
-  float total_period = 0.0; // used in calculating topping fluids
-  
-  if(color_C[index] != 0){
-    run_pump(0, color_C[index] * PIGMENT_PUMP_TIME_FACTOR);
-    period = color_C[index] * PIGMENT_PUMP_TIME_FACTOR;
-    total_period += period;
-    if(period > longest_period){
-      longest_period = period; 
-    }
-  }
-  if(color_M[index] != 0){
-    run_pump(1, color_M[index] * PIGMENT_PUMP_TIME_FACTOR);
-    period = color_M[index] * PIGMENT_PUMP_TIME_FACTOR;
-    total_period += period;
-    if(period > longest_period){
-      longest_period = period; 
-    }
-  }
-  if(color_Y[index] != 0){
-    run_pump(2, color_Y[index] * PIGMENT_PUMP_TIME_FACTOR);
-    period = color_Y[index] * PIGMENT_PUMP_TIME_FACTOR;
-    total_period += period;
-    if(period > longest_period){
-      longest_period = period; 
-    }
-  }
-  if(color_K[index] != 0){
-    run_pump(3, color_K[index] * PIGMENT_PUMP_TIME_FACTOR);
-    period = color_K[index] * PIGMENT_PUMP_TIME_FACTOR;
-    total_period += period;
-    if(period > longest_period){
-      longest_period = period; 
-    }
-  }
 
-    total_period += period;
+  float longest_period = 0; // 
+  float total_period = 0; // used in calculating topping fluids
+  ////Serial.print("DISPENDING: ");
+  ////Serial.println(index);
+  for(int i=0;i<4;i++){
+    if(generated_colors[index][i] != 0.0){
+      int period = generated_colors[index][i] * PIGMENT_PUMP_TIME_FACTOR;
+      run_pump(i, period);
+      total_period += period;
+      if(period > longest_period){
+        longest_period = period;
+      }
+    }
+    int total_color = 0;
+    for(int i=0; i<4; i++){
+      total_color += generated_colors[index][i];
+    }
+    //Prepare topping pump times, but set to a minus value so they won't run yet
+    for(int i=0;i<4;i++){
+      pump_timers[4+i] = -1 * (generated_colors[index][i] / (total_color / 100.0));
+    }
+  }
 
   switch(TOPPING_MODE){
-    case 1: // Start topping when pigments are done
-      topping_fluid_timer = millis() + longest_period;
+  case 1: // Start topping when pigments are done
+    time_until_topping = millis() + longest_period;
     break;
-    case 2: // Start topping after TOPPING_DELAY milliseconds
-      topping_fluid_timer = millis() + TOPPING_DELAY;
+  case 2: // Start topping after TOPPING_DELAY milliseconds
+    time_until_topping = millis() + TOPPING_DELAY;
     break;
-    case 3: // Start topping at the same time as pigments
-      topping_fluid_timer = 1; // Very small amount so topping starts emidiately
+  case 3: // Start topping at the same time as pigments
+    time_until_topping = 1; // Very small amount so topping starts immediately
     break;
   }
   // Set a timer for the topping fluid so it starts pouring as soon as the
   // last CMYK tube is closed
-  
+
   // Block the button for two seconds to prevent double-pouring
-  button_block_timer = millis() + BUTTON_BLOCK_TIME;
-  */
+  button_block_timer = millis() + BUTTON_BLOCK_PERIOD;
+
 }
 
 //==============================================================
@@ -285,9 +385,30 @@ void dispend(const int index){
 //==============================================================
 void run_pump(const int index, const int time_to_run){
   if(index < 8){ // make sure the index is valid
-    if(timers[index] == 0){
-      timers[index] = millis()+time_to_run;
+    if(pump_timers[index] == 0){
+      pump_timers[index] = millis()+time_to_run;
       digitalWrite(relay_pins[index], LOW);
     }    
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
